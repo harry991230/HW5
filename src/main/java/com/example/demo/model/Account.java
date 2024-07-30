39351691f8fc41988acf318c16c65fb09e04eea5
Harry <harrytsai999@gmail.com>
2024-07-30 12:07:03 +0800

    刪除不必要的檔案

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -28,50 +28,5 @@ public class Account {
 	
 	@Column(name="email")
 	private String email;
-	
-
-	public int getId() {
-		return id;
-	}
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	@Override
-	public String toString() {
-		return "Account [id=" + id + ", username=" + username + "]";
-	}
-	
 
 }
