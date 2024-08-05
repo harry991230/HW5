3708c3706c52cd857472d2a8b5b16ab75d78241e
Harry <harrytsai999@gmail.com>
2024-08-06 03:36:30 +0800

    modify: config

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO account (username, password, email) VALUES 
-('Harry', '******', 'harrytsai999@gmail.com'),
-('Winnie', '******', 'winnie@gmail.com'),
-('Keanu', '******', 'keanu@gmail.com'),
-('Rich', '******', 'rich@gmail.com');
